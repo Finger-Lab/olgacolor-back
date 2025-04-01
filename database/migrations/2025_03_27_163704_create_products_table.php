16d0805c37827e521e04f4a36711a3851e62bb3c
Mateus De Paula <mateus.dpaula10@gmail.com>
2025-04-01 14:51:56 -0300

    Filtra produtos por categoria e nome

--- database/migrations/2025_03_27_163704_create_products_table.php
+++ database/migrations/2025_03_27_163704_create_products_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->text('description');
-            $table->decimal('price', 8, 2);
-            $table->string('image');
+            $table->string('name')->nullable();
+            $table->string('image_big')->nullable();
+            $table->string('image_small')->nullable();
+            $table->string('category')->nullable();
             $table->timestamps();
         });
     }
